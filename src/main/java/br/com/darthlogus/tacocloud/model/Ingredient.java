487a9a23e03a733d3addb15ca22fde3c4235627f
Matheus Ribeiro <matheus.junio159@gmail.com>
2022-09-05 09:11:23 -0300

    Completion, Book Chapter 2, Spring MVC

---
+++ src/main/java/br/com/darthlogus/tacocloud/model/Ingredient.java
@@ -0,0 +1,15 @@
+package br.com.darthlogus.tacocloud.model;
+
+import lombok.Data;
+
+@Data
+public class Ingredient {
+
+    private final String id;
+    private final String name;
+    private final Type type;
+    
+    public enum Type {
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
+}
---
+++ src/test/java/br/com/darthlogus/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.darthlogus.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
